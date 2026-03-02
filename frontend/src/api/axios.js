9bec65f797a22f64e58be1a271417e0122922a24
Alberto Giuntelli <alberto.giuntelli@gmail.com>
2026-03-02 17:36:34 +0100

    Frontend pronto per Railway 2026

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://plusmarket.ddns.net:5000",
+    // Backend in locale: assicurati che il server giri su http://localhost:5000
+    baseURL: "http://localhost:5000",
     headers: {
         "Content-Type": "application/json",
     },
