0a5e22d8e621a3e68bda0ff2441a789a1e7e2e71
Alberto Giuntelli <alberto.giuntelli@gmail.com>
2026-03-04 06:48:12 +0100

    Aggiornamento frontend

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,8 @@
+// frontend/src/api.js
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "/api",
+    baseURL: "https://passionate-laughter-production-5fc7.up.railway.app",
 });
 
 export default api;
