b46c50c715231f62fbae02ccb209c35b6faaca07
Alberto Giuntelli <alberto.giuntelli@gmail.com>
2026-03-04 09:54:40 +0100

    update backend

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,18 @@
-// backend/server.js
 import express from 'express';
 import cors from 'cors';
 import authRoutes from './routes/authRoutes.js';
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 
+// ROUTES
 app.use('/auth', authRoutes);
 
+// PORTA PER RAILWAY
 const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
     console.log(`Server avviato sulla porta ${PORT}`);
 });
